2eed7c2189ceb9603aa9fa4f322d6fe37842f8bb
Andrea Zanin <azanin@fbk.eu>
2019-05-05 18:55:23 +0200

    Updates

---
+++ precache-manifest.ed514e2d66dd0739dc04898a85807316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06e95fdeddd27472cefb880f944e8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9392404ece9cac07c131",
+    "url": "/static/css/main.5d20aaf3.chunk.css"
+  },
+  {
+    "revision": "cbc8e83cf98b8d747f5e",
+    "url": "/static/js/2.7095d30c.chunk.js"
+  },
+  {
+    "revision": "9392404ece9cac07c131",
+    "url": "/static/js/main.c99898aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
